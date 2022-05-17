00ab7a85e61ea42b0429bb5a254740abcac1a94b
Anish Shah <93145909+ash0ts@users.noreply.github.com>
2022-05-17 03:00:42 -0400

    pipenv + prediction service update
    Add pipenv and update deployment scripts packaged with promoted model to match new form of wandb predictions service

--- tutorials/ner_drugs/scripts/deployment/predict.py
+++ tutorials/ner_drugs/scripts/deployment/predict.py
@@ -1,14 +1,18 @@
+from html import entities
 import typer
 import spacy
+from pydantic import BaseModel
+from typing import List, Dict
 
 
-def load_model(model_path):
+def load_wandb_model(model_path):
     nlp = spacy.load(model_path)
     return nlp
 
 
-def predict(nlp, txt):
+def predict(nlp, row):
     # TODO: Process prediction response for endpoint here
+    txt = row['text']
     doc = nlp(txt)
     ents = []
     for ent in doc.ents:
@@ -18,11 +22,24 @@ def predict(nlp, txt):
                 "label": ent.label_
             }
         )
-    return ents
+    pred = {
+        "text": txt,
+        "entities": ents
+    }
+    return pred
+
+
+class Request(BaseModel):
+    text: str
+
+
+class Response(BaseModel):
+    text: str
+    entities: list[dict]
 
 
 def main(txt: str):
-    nlp = load_model()
+    nlp = load_wandb_model()
     prediction = predict(nlp, txt)
     typer.echo(prediction)
 
